9123177d14c88f0e56fbf23d6857671556f871b7
TUYISENGE Samuel <92625609+semy-tuyi@users.noreply.github.com>
2022-08-01 10:06:48 +0200

    fix tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { render, fireEvent, cleanup } from "@testing-library/react";
 import App from '../App.js';
+import Login from '../views/Login'
+import Home from '../views/Home'
+import Nav from '../components/NavDummy'
 
 afterEach(cleanup);
 
 it('renders correctly', () => {
   const div = document.createElement('div');
   ReactDom.render(<App />, div);
+  ReactDom.render(<Home />, div);
+  ReactDom.render(<Login />, div);
+
 });
