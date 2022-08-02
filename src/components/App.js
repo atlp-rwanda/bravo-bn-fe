a182ea6073a7059ddad53fb6c518426614e1469a
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-08-02 13:11:12 +0200

    ft(google-facebook-login): implement google and facebook login interface

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,5 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-import Counter from "./Counter.js";
+import React from 'react';
+import SignupForm from './SignupForm'
 
-const App = () => {
-  return (
-    <div className="container">
-      <h1>Welcome to Barefoot Nomad Frontend</h1>
-      <Counter />
-    </div>
-  );
-};
-export default App;
+const App = () => <SignupForm />;
+export default App; 
--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,20 @@ module.exports = {
   module: {
    rules: [
     {
-      test: /\.js$/,
+      test: /\.(js|jsx)$/,
       exclude:/node_module/,
+      resolve: {
+        extensions: [".js", ".jsx"]
+      },
       use: {
         loader :'babel-loader'
       }
+    },{
+      test: /\.(jpe?g|gif|png|svg)$/i,
+      loader: 'url-loader',
+      options: {
+        limit: 25000,
+      },
     },
     {
       test: /\.s[ac]ss$/i,
@@ -34,6 +43,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin(
       {
+        favicon: "./src/assets/barefoot_small_logo.png",
         template: './src/index.html',
       }
     )
