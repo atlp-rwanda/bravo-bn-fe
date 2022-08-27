b9f271a803e8278c5a848a1fad77f6bc2e97a098
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-08-27 18:39:33 +0200

    ft(user-requests-page): fix navbar

--- src/components/dashboard/Dash.js
+++ src/components/dashboard/Dash.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import Contents from "./Contents";
-import Navbar from "./Navbar";
 import Footer from "./footer";
 
 const Dash = () => {
   return (
     <div className="dashboard-container">
-      <Navbar />
       <Contents />
       <Footer />
     </div>
